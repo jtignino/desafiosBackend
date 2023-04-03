650698445d3999977b7e573abb841bf0ac06b620
jtignino <jonathan.jat@hotmail.com>
2023-04-03 20:47:05 -0300

    Segundo desafio entregable. FINAL.

---
+++ clase--5/index.js
@@ -0,0 +1,19 @@
+import ProductManager from "./managers/productManager.js";
+
+const manager = new ProductManager('./files/Productos.json');
+
+const env = async () => {
+    let productos = await manager.getProducts();
+    // console.log(productos);
+    // console.log('-------------------------------');
+
+    const product = {
+        title: 'Update prueba',
+    };
+
+    // await manager.updateProduct(3, product);
+    // await manager.deleteProduct(1);
+
+}
+
+env()
